4fe95648b0155c259db6a5a37113fb0a02d4a7d3
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-08-23 15:07:51 -0500

    cheer still works with all the things

---
+++ cheer.js
@@ -0,0 +1,43 @@
+let name = "JOHN DOE";
+
+
+
+//use the following:
+// Object literal shorthand
+// Destructuring assignments
+
+const userName = {
+    first: "JOHN",
+    last: "DOE"
+}
+const { first } = userName;
+const { last } = userName;
+const vowelSoundSet = new Set(["O", "H"]);
+const consonantSoundSet = new Set(["J", "D"]);
+
+vowelSoundSet.add("N").add("E");
+
+let fullNameArr = [...first, ...last];
+
+let cheerVowel = (letter) => {
+    console.log(`Give me an ${letter}!`);
+}
+
+let cheerConson = (letter) => {
+    console.log(`Give me a ${letter}!`);
+}
+
+let makeCheer = (nameArray) => {
+   for (let i=0; i < nameArray.length; i++) {
+     if (vowelSoundSet.has(nameArray[i])) {
+         cheerVowel(nameArray[i]);
+     } else if (consonantSoundSet.has(nameArray[i])) {
+        cheerConson(nameArray[i]);
+     }
+   }
+   console.log("What does that spell?")
+   console.log(`${first} ${last}!`)
+}
+
+makeCheer(fullNameArr);
+
